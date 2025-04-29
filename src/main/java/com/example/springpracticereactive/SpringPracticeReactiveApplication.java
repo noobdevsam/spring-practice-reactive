3268068d68d22714f723db95654ef213a9f2a135
noobdevsam <mdshoykotislam05@gmail.com>
2025-04-29 23:26:38 +0600

    initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ src/main/java/com/example/springpracticereactive/SpringPracticeReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpracticereactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springpracticereactive/SpringPracticeReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpracticereactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
