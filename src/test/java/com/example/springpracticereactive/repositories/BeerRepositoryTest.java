07d437543a1a0e2a20baa8fe7b7d956225fc9cfe
noobdevsam <mdshoykotislam05@gmail.com>
2025-04-30 20:51:52 +0600

    import DatabaseConfig in BeerRepositoryTest for proper test configuration

--- src/test/java/com/example/springpracticereactive/repositories/BeerRepositoryTest.java
+++ src/test/java/com/example/springpracticereactive/repositories/BeerRepositoryTest.java
@@ -1,13 +1,16 @@
 package com.example.springpracticereactive.repositories;
 
+import com.example.springpracticereactive.config.DatabaseConfig;
 import com.example.springpracticereactive.domain.Beer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.context.annotation.Import;
 
 import java.math.BigDecimal;
 
 @DataR2dbcTest
+@Import(DatabaseConfig.class)
 class BeerRepositoryTest {
 
 	@Autowired
