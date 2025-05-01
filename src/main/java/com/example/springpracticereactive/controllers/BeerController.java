d4ac6a591137015935f2f0e8fc6e7c4035afeb78
noobdevsam <mdshoykotislam05@gmail.com>
2025-05-01 13:02:40 +0600

    add BeerController to use BeerService for listing beers

--- src/main/java/com/example/springpracticereactive/controllers/BeerController.java
+++ src/main/java/com/example/springpracticereactive/controllers/BeerController.java
@@ -1,6 +1,7 @@
 package com.example.springpracticereactive.controllers;
 
 import com.example.springpracticereactive.model.BeerDTO;
+import com.example.springpracticereactive.services.BeerService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -9,11 +10,14 @@ import reactor.core.publisher.Flux;
 public class BeerController {
 
 	public static final String BEER_PATH = "/api/v2/beer";
+	private final BeerService beerService;
+
+	public BeerController(BeerService beerService) {
+		this.beerService = beerService;
+	}
 
 	@GetMapping(BEER_PATH)
 	Flux<BeerDTO> listBeers() {
-		return Flux.just(
-			new BeerDTO(1), new BeerDTO(2)
-		);
+		return beerService.listBeers();
 	}
 }
