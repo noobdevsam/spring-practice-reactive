921ea88225dec49b26ffebac43192039682fb0ee
noobdevsam <mdshoykotislam05@gmail.com>
2025-05-01 16:35:50 +0600

    add getBeerById method to BeerService for retrieving a beer by its ID

--- src/main/java/com/example/springpracticereactive/services/BeerService.java
+++ src/main/java/com/example/springpracticereactive/services/BeerService.java
@@ -2,9 +2,12 @@ package com.example.springpracticereactive.services;
 
 import com.example.springpracticereactive.model.BeerDTO;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface BeerService {
 
 	Flux<BeerDTO> listBeers();
+
+	Mono<BeerDTO> getBeerById(Integer id);
 
 }
