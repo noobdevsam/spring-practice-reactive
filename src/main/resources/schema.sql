911c4a48d934f680117ca4f78ac46a2fcd3c3804
noobdevsam <mdshoykotislam05@gmail.com>
2025-05-03 18:13:57 +0600

    refactor schema.sql to improve beer table definition and add customer table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,27 +1,19 @@
 CREATE TABLE if NOT EXISTS beer
 (
-    id
-    integer
-    NOT
-    NULL
-    PRIMARY
-    KEY
-    AUTO_INCREMENT,
-    beer_name
-    varchar
-(
-    255
-),
-    beer_style varchar
-(
-    255
-),
-    upc varchar
-(
-    25
-),
+    id         integer NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    beer_name  varchar(255),
+    beer_style varchar(255),
+    upc        varchar(25),
     quantity_on_hand integer,
     price decimal,
     created_date timestamp,
     last_modified_date timestamp
-    );
+);
+
+create table if not exists customer
+(
+    id                 integer not null primary key auto_increment,
+    customer_name      varchar(255),
+    created_date       timestamp,
+    last_modified_date timestamp
+);
