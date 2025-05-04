d0b8c4599bbafd62000a8f555715d7f1668be04f
noobdevsam <mdshoykotislam05@gmail.com>
2025-05-04 21:41:04 +0600

    add test for retrieving beer by ID in BeerControllerTest

--- src/test/java/com/example/springpracticereactive/controllers/BeerControllerTest.java
+++ src/test/java/com/example/springpracticereactive/controllers/BeerControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.springpracticereactive.controllers;
 
+import com.example.springpracticereactive.model.BeerDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
@@ -27,5 +28,15 @@ class BeerControllerTest {
 		 * URI (BeerController.BEER_PATH) and returns a ResponseSpec object. This object allows further assertions
 		 * to be made on the response, such as verifying the status code, headers, and body content.
 		 * */
+	}
+	
+	@Test
+	void test_get_beer_by_id() {
+		webTestClient.get()
+			.uri(BeerController.BEER_PATH_ID, 1)
+			.exchange()
+			.expectStatus().isOk()
+			.expectHeader().valueEquals("content-type", "application/json")
+			.expectBody(BeerDTO.class);
 	}
 }
