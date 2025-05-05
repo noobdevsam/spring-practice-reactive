e9f7bb3867c4252bbe65d3067da9b7792a431b8d
noobdevsam <mdshoykotislam05@gmail.com>
2025-05-05 20:18:06 +0600

    add: implement test_create_new_beer_with_bad_data in BeerControllerTest and update BeerDTO constructor

--- src/main/java/com/example/springpracticereactive/model/BeerDTO.java
+++ src/main/java/com/example/springpracticereactive/model/BeerDTO.java
@@ -23,9 +23,9 @@ public record BeerDTO(
 	LocalDateTime createdDate,
 	LocalDateTime lastModifiedDate
 ) {
-	//	public BeerDTO(Integer id) {
-//		this(id, null, null, null, null, null, null, null);
-//	}
+	public BeerDTO(String beerName) {
+		this(null, beerName, null, null, null, null, null, null);
+	}
 //
 //	public BeerDTO(Integer id, String beerName, String beerStyle, String upc, String quantityOnHand, BigDecimal price) {
 //		this(id, beerName, beerStyle, upc, quantityOnHand, price, null, null);
