3beb46e2e6d216287e32086c266ed69ea092ff2c
noobdevsam <mdshoykotislam05@gmail.com>
2025-05-05 08:48:19 +0600

    add: enhance CustomerControllerTest with method ordering and WebTestClient integration

--- src/test/java/com/example/springpracticereactive/controllers/CustomerControllerTest.java
+++ src/test/java/com/example/springpracticereactive/controllers/CustomerControllerTest.java
@@ -1,11 +1,31 @@
 package com.example.springpracticereactive.controllers;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
+@SpringBootTest
+@AutoConfigureWebTestClient
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CustomerControllerTest {
 	
+	@Autowired
+	WebTestClient webTestClient;
+	
+	@Order(1)
 	@Test
 	void listCustomers() {
+		webTestClient.get()
+			.uri(CustomerController.CUSTOMER_PATH)
+			.exchange()
+			.expectStatus().isOk()
+			.expectHeader().valueEquals("content-type", "application/json")
+			.expectBody().jsonPath("$.size()").isEqualTo(3);
 	}
 	
 	@Test
