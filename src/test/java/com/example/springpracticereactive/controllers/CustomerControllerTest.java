63b5ffcf023e26757992dd8c1e84648cdf6527b8
noobdevsam <mdshoykotislam05@gmail.com>
2025-05-05 08:42:28 +0600

    add: create CustomerControllerTest with initial test methods

---
+++ src/test/java/com/example/springpracticereactive/controllers/CustomerControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.springpracticereactive.controllers;
+
+import org.junit.jupiter.api.Test;
+
+class CustomerControllerTest {
+	
+	@Test
+	void listCustomers() {
+	}
+	
+	@Test
+	void getCustomerById() {
+	}
+	
+	@Test
+	void createNewCustomer() {
+	}
+	
+	@Test
+	void updateCustomer() {
+	}
+	
+	@Test
+	void deleteCustomerById() {
+	}
+}
