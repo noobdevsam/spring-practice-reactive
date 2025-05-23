cb52dffed724fb2ecfc9024de0edc0aed670ffef
noobdevsam <mdshoykotislam05@gmail.com>
2025-05-23 17:17:16 +0600

    add: implement security configuration for OAuth2 resource server

---
+++ src/main/java/com/example/springpracticereactive/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.springpracticereactive.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+	
+	@Bean
+	SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+		return http
+			       .authorizeExchange(
+				       exchange -> exchange.anyExchange().authenticated()
+			       )
+			       .oauth2ResourceServer(
+				       oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults())
+			       )
+			       .build();
+	}
+}
