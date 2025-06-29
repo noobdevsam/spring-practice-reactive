363b4210a9be10ef4e8e382bd3dde71bb8d8a5a1
noobdevsam <mdshoykotislam05@gmail.com>
2025-06-29 22:04:54 +0600

    add: enhance BeerMapper interface with JavaDoc comments for improved code documentation

--- src/main/java/com/example/springpracticereactive/mappers/BeerMapper.java
+++ src/main/java/com/example/springpracticereactive/mappers/BeerMapper.java
@@ -4,10 +4,26 @@ import com.example.springpracticereactive.domain.Beer;
 import com.example.springpracticereactive.model.BeerDTO;
 import org.mapstruct.Mapper;
 
+/**
+ * Mapper interface for converting between Beer and BeerDTO objects.
+ * Utilizes MapStruct for automatic mapping implementation.
+ */
 @Mapper(componentModel = "spring")
 public interface BeerMapper {
 
-	Beer beerDTOToBeer(BeerDTO beerDTO);
+    /**
+     * Converts a BeerDTO object to a Beer entity.
+     *
+     * @param beerDTO The BeerDTO object to be converted.
+     * @return The corresponding Beer entity.
+     */
+    Beer beerDTOToBeer(BeerDTO beerDTO);
 
-	BeerDTO beerToBeerDTO(Beer beer);
+    /**
+     * Converts a Beer entity to a BeerDTO object.
+     *
+     * @param beer The Beer entity to be converted.
+     * @return The corresponding BeerDTO object.
+     */
+    BeerDTO beerToBeerDTO(Beer beer);
 }
